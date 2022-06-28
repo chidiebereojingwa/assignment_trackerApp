e0c4fd854aaec7d36d3848785d4deb454382bf9d
chidiebereojingwa <ojingwaoji@gmail.com>
2022-06-28 23:21:41 +0100

    Add routes for workout

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-require('dotenv').config
-const express = require("express")
+require("dotenv").config();
 
+const express = require("express");
+const workoutRoutes = require("./routes/workouts");
 
 // express app
-
 const app = express();
 
+// middleware
+app.use(express.json());
 
-// routes 
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
-app.get('/', (req,res)=> {
-    res.json({mssg: "Welcome to the app"})
-})
+// routes
+app.use("/api/workouts", workoutRoutes);
 
-
-// listen for requests 
-app.listen(process.env.PORT,() => {
-    console.log("listening on port 4000", process.env.PORT);
-})
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log("listening on port", process.env.PORT);
+});
