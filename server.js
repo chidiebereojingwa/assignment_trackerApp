b8a494c7666db2a7ec77d4294081bc0e373f2689
chidiebereojingwa <ojingwaoji@gmail.com>
2022-06-29 22:18:53 +0100

    debug

--- server.js
+++ server.js
@@ -19,13 +19,14 @@ app.use((req, res, next) => {
 app.use("/api/workouts", workoutRoutes);
 
 // connect to db
-mongoose.connect(process.env.MONG_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    // listen for requests
+    console.log("connected to database");
+    // listen to port
     app.listen(process.env.PORT, () => {
-      console.log("connected to the server and listening on port", process.env.PORT);
+      console.log("listening for requests on port", process.env.PORT);
     });
   })
-  .catch((error) => {
-    console.log(error);
+  .catch((err) => {
+    console.log(err);
   });
